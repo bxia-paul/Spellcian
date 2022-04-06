bd1ed6426c2726d7d86c430dfd8453cfaa42aad0
bxia-paul <92060355+bxia-paul@users.noreply.github.com>
2022-04-05 22:37:35 -0400

    Update word.py

--- App/controllers/word.py
+++ App/controllers/word.py
@@ -3,7 +3,7 @@ from App.database import db
 import csv
 
 def loadWords():
-   word_file = csv.DictReader(open("/workspace/Spellcian/App/spellican_word_list.csv", encoding='utf-8-sig'))
+   word_file = csv.DictReader(open("App/spellican_word_list.csv", encoding='utf-8-sig'))
    
    for w in word_file: # creates an object from the Word model
     db.session.add(Word(
